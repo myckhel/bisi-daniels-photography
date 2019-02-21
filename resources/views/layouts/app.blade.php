f067a0ab7a1e5755243d347e8c40cd8573e441be
myckhel <myckhel1@hotmail.com>
2019-02-21 23:58:35 +0100

    added loader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,21 @@
 
 </head>
 <body>
+
+      <!-- Preloader -->
+      <div id="preloader">
+          <div class="showbox">
+              <div class="loader">
+                  <svg class="circular" viewBox="25 25 50 50">
+                      <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+                  </svg>
+              </div>
+          </div>
+          <div class="questions-area text-center">
+
+
+          </div>
+      </div>
     <!-- Preloader -->
     <!-- <div id="preloader">
         <div class="showbox">
