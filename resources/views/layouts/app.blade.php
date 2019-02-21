6bf08177825e992f0b86f92f6ec66e5b2b2ec30e
myckhel <myckhel1@hotmail.com>
2019-02-21 23:38:01 +0100

    fixed app bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{@yield('title')}} | {{ config('app.name') }}</title>
+    <title>@yield('title') | {{ config('app.name') }}</title>
 
     <link rel="shortcut icon" href="{{ URL::asset('img/core-img/favicon.ico') }}">
 
