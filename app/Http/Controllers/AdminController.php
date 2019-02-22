77be828d56a536b52a1a3ac5eebccc55eefc9b22
myckhel <myckhel1@hotmail.com>
2019-02-22 01:23:03 +0100

    removed role policy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
   public function __construct()
   {
       $this->middleware('auth');
-      $this->middleware('role');
+      // $this->middleware('role');
   }
     //
 
